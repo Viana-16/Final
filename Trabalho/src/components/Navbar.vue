83c4fb70f1c73b5676bd78a421b2bc3a986686c4
Viana-16 <viniciusvribeiro85@gmail.com>
2024-11-11 13:57:01 -0300

    navbar

---
+++ Trabalho/src/components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="nav">
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/Jogos">Jogos</RouterLink>
+    <RouterLink to="/Times">Times</RouterLink>
+    <RouterLink to="/Campeonatos">Campeonatos</RouterLink>
+    
+
+
+</div>
+
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+#nav{
+    background-color: white;
+    height: 75px; 
+    /* justify-content: flex-end;
+    align-items: center; */
+}
+
+#nav a{
+    color: black;
+    margin: 12px;
+}
+
+</style>
