d175972a1edb36bfed563fdb8e9849769af82681
duda-1 <163910040+pedrofelipe09@users.noreply.github.com>
2024-11-11 14:47:37 -0300

    Teste

--- Trabalho/src/views/HomeView.vue
+++ Trabalho/src/views/HomeView.vue
@@ -1,5 +1,13 @@
 <template>
+    <div>
+    <h1>Principais Notícias</h1>
 
+    
+Fifa define data do sorteio dos grupos para o Mundial de Clubes de 2025
+
+A Fifa divulgou, nesta segunda-feira, que sorteará os grupos para o Mundial de Clubes de 2025 
+no dia 5 de dezembro, em Miami. A competição acontecerá entre os dias 15...
+</div>
 </template>
 
 <script>
