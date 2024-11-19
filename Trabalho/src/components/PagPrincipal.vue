6e9c3f515fb4ffcb38a5e1e46a0f740f0e236c5a
duda-1 <163910040+pedrofelipe09@users.noreply.github.com>
2024-11-19 14:19:07 -0300

    Update5

--- Trabalho/src/components/PagPrincipal.vue
+++ Trabalho/src/components/PagPrincipal.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        Top news Brazilians 
+        Main news from Brazil
         <div class="reportagem">
             <div class="reportagem-card" id="card">
                 <h2 class="reportagem-card-title">Atlético e Botafogo Fazem final da libertadores</h2>
@@ -21,7 +21,7 @@
             </div>
         </div>
         <div class="reports2">
-            <div class="reportes2-card" id="card1">
+            <div class="reports2-card" id="card1">
                 <h2 class="reports2-card-title">Ainda em transição, Luiz Araújo está fora de Cuiabá x Flamengo</h2>
                  o Flamengo tem mais um desfalque confirmado para o duelo com o Cuiabá, nesta quarta-feira, às 19h 
                 (de Brasília).
@@ -31,11 +31,29 @@
                 <h2 class="reports2-card-title">Memphis publica vídeo de incentivo a jogador do Corinthians</h2>
                 Memphis Depay usou as redes sociais para incentivar Ruan Oliveira durante o longo processo de recuperação no Corinthians.
             </div>
-            Brazilians Shorts
+           
+        </div>
+        
+            Lances Brazilian football
+
+        <div class="videosBrasil">
+            <div class="videosBrasil-card" id="card1">
+                <h2 class="videosBrasil-card-title">Melhores Momentos Atlético-MG X Botafogo</h2>
+            </div>
+            <div class="videosBrasil-card" id="card2">
+                <h2 class="videosBrasil-card-title">Melhores Momentos Cruzeiro X Corinthians</h2>
+            </div>
+            <div class="videosBrasil-card" id="card3">
+                <h2 class="videosBrasil-card-title">Melhores Momentos Bahia X Palmeiras</h2>
+            </div>
+            <div class="videosBrasil-card" id="card4">
+                <h2 class="videosBrasil-card-title">Melhores Momentos Fortaleza X Flamengo</h2>
+            </div>
         </div>
 
-
-    </div>
+        Main news from European
+        
+        </div>
 </template>
 
 
@@ -45,4 +63,15 @@ export default{
 }
 </script>
 
-
+<style scoped>
+/* .reportagem-card {
+    max-width: 400px; 
+    border: 1px solid #ddd; 
+    border-radius: 10px;
+    overflow: hidden;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    font-family: Arial, sans-serif;
+    transition: transform 0.3s, box-shadow 0.3s;
+    background-color: #fff;
+} */
+</style>
